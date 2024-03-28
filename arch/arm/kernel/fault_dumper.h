4005270cd5e13958a724aaa7d6952d4ee80fb96b
William Mokhlef <william.mokhlef1@hitachienergy.com>
2024-03-28 16:50:07 +0100

    Merged PR 10094: Create Hoookpoint in arm_notify_die and create a module that can be installed to it Create the ability to specify where the different callbacks should place their information in combination with the ability to specify different memory ranges from which the registers and/or information should be retrieved from during insertion of the module. insmod <kernel-module>.ko dumper_init_info=<destination-memory>,<length>,<source-memory1>,<length1>,<source-memory2>,<length2>,...,<source-memoryX>,<source-memoryX> The capability is enabled through CONFIG_PG_HAVE_FAULT_DUMPER=m. Related work items: #59539 #59540 https://dev.azure.com/PG-CT/PGLinux/_git/pg-linux/pullrequest/10094
    Signed-off-by: Piotr Lewicki <piotr.l.lewicki@hitachienergy.com>
    
    %% original patch: 0029-Merged-PR-10094-Create-Hoookpoint-in-arm_notify_die-.patch

---
+++ arch/arm/kernel/fault_dumper.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2024 Hitachi Energy. All rights reserved.
+ */
+
+enum FAULT_ENTRY_DUMPER {
+	/* START 		traps.c */
+	FAULT_ENTRY_ARM_DIE = 0,
+};
+
+// The length looks to be enough based on where arm_notify_die is called
+#define MAX_LEN_STR_ARM_DIE 40
+struct arm_die_entry {
+	const char *str;
+	char make_additional_space_for_str[MAX_LEN_STR_ARM_DIE - sizeof(const char *)];
+	int signo; 
+	int si_code;
+	void *addr;
+	unsigned long err;
+	unsigned long trap;
+};
+
+struct fault_callback_info
+{
+	union {
+		struct arm_die_entry 		arm_die;
+	} entry_type_u;
+
+	int which_entry;
+	size_t len_entry;
+	void* handle;
+};
+
+struct fault_callback
+{
+	void (*fault_dump_persist)(struct fault_callback *fault_dumper);
+	struct fault_callback_info info;
+};
+
+int fault_dump_register(struct fault_callback* callback);
+int fault_dump_unregister(struct fault_callback* callback);
+struct fault_callback* get_current_fault_callback(void);
